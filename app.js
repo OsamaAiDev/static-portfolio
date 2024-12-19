45e0f097b3f019a10cfecec3e507796823882bbe
OsamaAiDev <149548703+OsamaAiDev@users.noreply.github.com>
2024-12-19 19:45:13 +0500

    added cv

--- app.js
+++ app.js
@@ -21,3 +21,24 @@ menu.onclick = () => {
   menu.classList.toggle("bx-x");
   navbar.classList.toggle("open");
 };
+
+// real time
+
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById("real-time").innerHTML = h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  } // add zero in front of numbers < 10
+  return i;
+}
+
+startTime();
